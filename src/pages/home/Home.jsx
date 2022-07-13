15d738b852c978c09b58fe6cee8b6f96643dfb62
esadakman <esadd26@gmail.com>
2022-07-13 14:46:38 +0300

    favicon added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,11 +6,8 @@ import CardArea, { LoadingDiv, Section } from "./Home.styled";
 // import NotFound from "../notfound/NotFound";
 import loading from "../../assets/loading.svg";
 import { AboutTitle } from "../about/About.styled";
-// const API_ID = "dec1c9ef";
-// const API_KEY = "8973ca575935d18beea52f4ab0a61a59";
 
 const Home = () => {
-  console.log(process.env);
   const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
   const [query, setQuery] = useState(
     ""
@@ -25,7 +22,7 @@ const Home = () => {
 
   // const [error, setError] = useState(false);
   // ! API URL --------------------
-  // const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
+
   const url = `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&mealType=${meal}`;
 
   const getRecipes = async () => {
